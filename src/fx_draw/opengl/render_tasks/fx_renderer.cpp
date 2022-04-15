b7cd80444b42205787c2413131255bca3eccfec8
Green Sky <green@g-s.xyz>
2022-04-15 19:45:08 +0200

    update for new entt

--- src/fx_draw/opengl/render_tasks/fx_renderer.cpp
+++ src/fx_draw/opengl/render_tasks/fx_renderer.cpp
@@ -29,9 +29,18 @@ FXDrawRenderTask::FXDrawRenderTask(MM::Engine& engine) : _fx_draw{engine} {
 
 void FXDrawRenderTask::render(MM::Services::OpenGLRenderer& rs, MM::Engine& engine) {
 	ZoneScopedN("fx_draw::FXDrawRenderTask::render");
-	auto& scene = engine.getService<MM::Services::SceneServiceInterface>().getScene();
+	auto* ssi = engine.tryService<MM::Services::SceneServiceInterface>();
+	if (ssi == nullptr) {
+		return; // nothing to draw
+	}
 
-	_fx_draw.setCamera(scene.ctx<MM::OpenGL::Camera3D>());
+	auto& scene = ssi->getScene();
+
+	if (!scene.ctx().contains<MM::OpenGL::Camera3D>()) {
+		return; // nothing to draw
+	}
+
+	_fx_draw.setCamera(scene.ctx().at<MM::OpenGL::Camera3D>());
 
 	rs.targets[target_fbo]->bind(MM::OpenGL::FrameBufferObject::W);
 
